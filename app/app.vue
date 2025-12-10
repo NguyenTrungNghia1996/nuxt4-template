0e11e9d43fbdf61ab2c64c3cea3dfab80fac8ec4
NguyenTrungNghia <nghiabgt@gmail.com>
2025-12-10 10:32:03 +0700

    update

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,25 @@
 <template>
-  <div>
-    <div class="bg-red-400">123</div>
-    <!-- <NuxtRouteAnnouncer />
-    <NuxtWelcome /> -->
-  </div>
+  <a-extract-style>
+    <a-config-provider :locale="locale" :theme="theme">
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </a-config-provider>
+  </a-extract-style>
 </template>
+<script setup>
+import vi_VN from "ant-design-vue/es/locale/vi_VN";
+const locale = computed(() => {
+  return vi_VN;
+});
+const theme = ref({});
+// const theme = ref({
+//   token: {
+//     colorPrimary: "#0057A0", // Màu xanh dương đậm trong logo – làm màu chủ đạo
+//     colorSuccess: "#7AC943", // Màu xanh lá logo – dùng cho thành công (Success)
+//     colorWarning: "#F59E0B", // Vàng cam nhẹ – cảnh báo nhưng vẫn hài hòa với logo
+//     colorError: "#DC2626", // Đỏ đậm hiện đại – phù hợp cho trạng thái lỗi
+//     colorInfo: "#0284C7",
+//   },
+// });
+</script>
