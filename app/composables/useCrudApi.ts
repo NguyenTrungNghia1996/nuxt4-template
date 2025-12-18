a8c2b3292529d29687d8c038ff193f806c38800a
NguyenTrungNghia <nghiabgt@gmail.com>
2025-12-18 16:59:29 +0700

    update

--- app/composables/useCrudApi.ts
+++ app/composables/useCrudApi.ts
@@ -13,17 +13,32 @@ export const useCrudApi = (endpoint: string) => {
     get(options?: RequestOptions) {
       return request.get(endpoint, options ?? {});
     },
+    getByRest(id: string | number, options?: RequestOptions) {
+      return request.get(`${endpoint}/${id}`, options ?? {});
+    },
     post(options?: RequestOptions) {
       return request.post(endpoint, options ?? {});
+    },
+    postByRest(id: string | number, options?: RequestOptions) {
+      return request.post(`${endpoint}/${id}`, options ?? {});
     },
     put(options?: RequestOptions) {
       return request.put(endpoint, options ?? {});
     },
+    putByRest(id: string | number, options?: RequestOptions) {
+      return request.put(`${endpoint}/${id}`, options ?? {});
+    },
     patch(options?: RequestOptions) {
       return request.patch(endpoint, options ?? {});
     },
+    patchByRest(id: string | number, options?: RequestOptions) {
+      return request.patch(`${endpoint}/${id}`, options ?? {});
+    },
     delete(options?: RequestOptions) {
       return request.delete(endpoint, options ?? {});
+    },
+    deleteByRest(id: string | number, options?: RequestOptions) {
+      return request.delete(`${endpoint}/${id}`, options ?? {});
     },
   };
 };
